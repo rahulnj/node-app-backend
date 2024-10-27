d3b7cd124af02f37c1e9e6a6057dfb1b41281ae1
rahulnj~ <rahuljayaraman8@gmail.com>
2024-10-27 17:42:58 +0530

    Removed the test script tag, will be added later

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,11 @@ import express, { Request, Response } from 'express';
 const app = express();
 
 app.use('/test', (req: Request, res: Response) => {
-  res.send('Hello World');
+  res.send('Hello World 1');
+});
+
+app.use('/test/1', (req: Request, res: Response) => {
+  res.send('Hello World 2');
 });
 
 app.listen(3000, () => {
