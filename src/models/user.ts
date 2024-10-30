01f8b7c05b5e7c10dfc062afb762ce2ef7fbf0e0
rahulnj~ <rahuljayaraman8@gmail.com>
2024-10-30 18:41:11 +0530

    Added : userSchema

---
+++ src/models/user.ts
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  passWord: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+});
+
+export const User = mongoose.model('User', userSchema);
