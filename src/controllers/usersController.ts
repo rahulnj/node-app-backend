4920b4edc3e143afa6904dd9d92b0f47e2f19d49
rahulnj~ <rahuljayaraman8@gmail.com>
2024-11-09 15:40:41 +0530

    (feat) get all users

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,14 +1,19 @@
 import { Request, Response } from 'express';
 import { User } from '@Models/user';
 
-export const getUsers = (req: Request, res: Response) => {
-  res.send('getUsers');
+export const getUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (error) {
+    console.error(error);
+    res.status(400).send(`An error occurred while fetching users:${error}`);
+  }
 };
 
 export const createUser = async (req: Request, res: Response) => {
   const { body } = req;
   const user = new User(body);
-
   try {
     await user.save();
     res.send(user);
