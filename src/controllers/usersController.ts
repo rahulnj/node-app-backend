4c350d7aafa6a7eebe41f4add102298a6e64e8c6
rahulnj~ <rahuljayaraman8@gmail.com>
2025-01-06 22:16:00 +0530

    (improvment) support fetching multiple users in the users api.

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -8,18 +8,37 @@ export const getUsers = async (
   next: NextFunction
 ) => {
   const { userId } = req.query;
+  const loggedInUserId = req.user._id;
+
   try {
-    logger.info(`Request started fetching profile details of ${userId}`);
-    const userProfile = await User.findById(userId).exec();
-    if (!userProfile) {
-      logger.warn(`No user found for ID: ${userId}`);
-      res.status(404).send('No user found');
+    if (userId) {
+      logger.info(`Request started fetching profile of user ID: ${userId}`);
+
+      const userProfile = await User.findById(userId).exec();
+
+      if (!userProfile) {
+        logger.warn(`No user found for ID: ${userId}`);
+        res.status(404).json({ message: 'No user found' });
+        return;
+      }
+
+      logger.info(`Successfully fetched profile details of user ID: ${userId}`);
+      res.status(200).json(userProfile);
       return;
     }
-    logger.info(
-      `Request completed successfully fetched profile details of ${userId}`
-    );
-    res.send(userProfile);
+
+    logger.info('Request started fetching all user profiles');
+
+    const allUsers = await User.find({ _id: { $ne: loggedInUserId } }).exec();
+
+    if (!allUsers.length) {
+      logger.warn('No users found');
+      res.status(404).json({ message: 'No users found' });
+      return;
+    }
+
+    logger.info('Successfully fetched all user profiles');
+    res.status(200).json(allUsers);
   } catch (error) {
     next(error);
   }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 
 import authRouter from '@Routes/auth';
-import profileRouter from '@Routes/users';
+import userRouter from '@Routes/users';
 import connectionRouter from '@Routes/connections';
 
 const apiRouter = Router();
 
 apiRouter.use('/auth', authRouter);
-apiRouter.use('/users', profileRouter);
+apiRouter.use('/users', userRouter);
 apiRouter.use('/connections', connectionRouter);
 
 export default apiRouter;
